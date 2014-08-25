c495bf83a1b93729bfc4960c59adbd38ba606f70
TheSneakyNarwhal <Thesneakynarwhal@hotmail.com>
2014-08-24 19:06:20 -0700

    Build

--- build/web/Items/Enum.dart
+++
@@ -1,9 +0,0 @@
-library ENUM;
-
-abstract class Enum<T,N>
-{
-  final T value;
-  final N NAME;
-  const Enum(this.value, this.NAME);
-  toString() => "${this.value}";
-}
--- build/web/Items/Weapon/Weapon.dart
+++
@@ -1,9 +0,0 @@
-library WEAPON;
-
-import '../Item/Item.dart';
-
-class Weapon extends Item
-{
-  int atk;
-  Weapon(int x, int y, var type) : super(x, y, type);
-}
