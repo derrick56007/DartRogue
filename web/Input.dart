c6c3055bda2893d4f9687c7500aa0bb11922a1d1
TheSneakyNarwhal <Thesneakynarwhal@hotmail.com>
2014-08-25 12:00:43 -0700

    NumPad support, keys are now holdable
    you can now hold down keys and you can use the numbed for movement now

--- web/Input.dart
+++ web/Input.dart
@@ -2,95 +2,136 @@ library INPUT;
 
 import 'dart:html';
 import 'Game.dart';
+import 'dart:async';
 
 class Input
 {
+  List<int> keys = [];
+  Timer timer;
+  
   Input()
   {
-    window.onKeyDown.listen((KeyboardEvent e){ keyPressed(e.keyCode);});
+    window.onKeyDown.listen((KeyboardEvent e){ keyDownHandler(checkValidKey(e.keyCode));});
+    window.onKeyUp.listen((KeyboardEvent e){ keyUpHandler(checkValidKey(e.keyCode));});
+  }
+  
+  int checkValidKey(int e)
+  {
+    if(e == KeyCode.NUM_SEVEN || e == KeyCode.NUM_EIGHT || e == KeyCode.NUM_NINE || e == KeyCode.NUM_SIX || e == KeyCode.NUM_THREE || e == KeyCode.NUM_TWO || e == KeyCode.NUM_ONE || e == KeyCode.NUM_FOUR || e == KeyCode.NUM_FIVE|| e == KeyCode.Q || e == KeyCode.W || e == KeyCode.E || e == KeyCode.D || e == KeyCode.C || e == KeyCode.X || e == KeyCode.Z || e == KeyCode.A || e == KeyCode.S)
+    {
+      return e;
+    }
+    return 0;
+  }
+  
+  void keyUpHandler(int e)
+  {
+    if(keys.contains(e))
+    {
+      keys.remove(e);
+      if(timer != null) timer.cancel();
+      if(keys.length > 0) timer = new Timer.periodic(const Duration(milliseconds: 100), (d){keyPressed(keys.last);});
+    }
+  }
+  
+  void keyDownHandler(int e)
+  {
+    if(!keys.contains(e))
+    {
+      keyPressed(e);
+      keys.add(e);
+      if(timer != null) timer.cancel();
+      timer = new Timer.periodic(const Duration(milliseconds: 100), (d){keyPressed(e);});
+    }
   }
 
   void keyPressed (int e)
   {
     print(new DateTime.now());
-    if(e == KeyCode.SEVEN || e == KeyCode.EIGHT || e == KeyCode.NINE || e == KeyCode.SIX || e == KeyCode.THREE || e == KeyCode.TWO || e == KeyCode.ONE || e == KeyCode.FOUR || e == KeyCode.Q || e == KeyCode.W || e == KeyCode.E || e == KeyCode.D || e == KeyCode.C || e == KeyCode.X || e == KeyCode.Z || e == KeyCode.A || e == KeyCode.S)
+
+    int moveY = 0;
+    int moveX = 0;
+    switch (e)
     {
-      int moveY = 0;
-      int moveX = 0;
-      switch (e)
-      {
-        case KeyCode.EIGHT: //UP
-          moveY = -1;
-          break;
-        case KeyCode.W:
-          moveY = -1;
-          break;
-          
-        case KeyCode.TWO: //DOWN
-          moveY = 1;
-          break;
-        case KeyCode.X:
-          moveY = 1;
-          break;
-          
-        case KeyCode.FOUR: //LEFT
-          moveX = -1;
-          break;
-        case KeyCode.A:
-          moveX = -1;
-          break;
-          
-        case KeyCode.SIX: //RIGHT
-          moveX = 1;
-          break;
-        case KeyCode.D:
-          moveX = 1;
-          break;
-          
-        ////////////////////////
-          
-        case KeyCode.SEVEN: //UP LEFT
-          moveY = -1;
-          moveX = -1;
-          break;
-        case KeyCode.Q:
-          moveY = -1;
-          moveX = -1;
-          break;
-          
-        case KeyCode.NINE: //UP RIGHT
-          moveY = -1;
-          moveX = 1;
-          break;
-        case KeyCode.E:
-          moveY = -1;
-          moveX = 1;
-          break;
-          
-        case KeyCode.THREE: //DOWN RIGHT
-          moveY = 1;
-          moveX = 1;
-          break;
-        case KeyCode.C:
-          moveY = 1;
-          moveX = 1;
-          break;
-          
-        case KeyCode.ONE: //DOWN LEFT
-          moveY = 1;
-          moveX = -1;
-          break;
-        case KeyCode.Z:
-          moveY = 1;
-          moveX = -1;
-          break;
-          
-        default:
-          break;
-      }
-      world.player.movePlayer(moveX, moveY);
-      display.displayWorld();
-      print(new DateTime.now());
+      case KeyCode.NUM_EIGHT: //UP
+        moveY = -1;
+        break;
+      case KeyCode.W:
+        moveY = -1;
+        break;
+        
+      case KeyCode.NUM_TWO: //DOWN
+        moveY = 1;
+        break;
+      case KeyCode.X:
+        moveY = 1;
+        break;
+        
+      case KeyCode.NUM_FOUR: //LEFT
+        moveX = -1;
+        break;
+      case KeyCode.A:
+        moveX = -1;
+        break;
+        
+      case KeyCode.NUM_SIX: //RIGHT
+        moveX = 1;
+        break;
+      case KeyCode.D:
+        moveX = 1;
+        break;
+        
+      ////////////////////////
+        
+      case KeyCode.NUM_SEVEN: //UP LEFT
+        moveY = -1;
+        moveX = -1;
+        break;
+      case KeyCode.Q:
+        moveY = -1;
+        moveX = -1;
+        break;
+        
+      case KeyCode.NUM_NINE: //UP RIGHT
+        moveY = -1;
+        moveX = 1;
+        break;
+      case KeyCode.E:
+        moveY = -1;
+        moveX = 1;
+        break;
+        
+      case KeyCode.NUM_THREE: //DOWN RIGHT
+        moveY = 1;
+        moveX = 1;
+        break;
+      case KeyCode.C:
+        moveY = 1;
+        moveX = 1;
+        break;
+        
+      case KeyCode.NUM_ONE: //DOWN LEFT
+        moveY = 1;
+        moveX = -1;
+        break;
+      case KeyCode.Z:
+        moveY = 1;
+        moveX = -1;
+        break;
+        
+      default:
+        break;
     }
+    world.player.movePlayer(moveX, moveY);
+    display.displayWorld();
+    print(new DateTime.now());
   }
+  
+  /*void delay()
+  {
+    var future = new Future.delayed(const Duration(milliseconds: 1000));
+    var subscription = future.asStream().listen((e){print("derp");});
+// ...
+    //subscription.cancel();
+  }*/
 }
